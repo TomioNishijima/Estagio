f53acc84c8ec45091dabff660c61310149e868db
TomioNishijima <162518617+TomioNishijima@users.noreply.github.com>
2026-06-06 17:49:45 -0300

    Delete demo directory

--- demo/src/main/java/CRUD/demo/repository/MovimentacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package CRUD.demo.repository;
-
-import CRUD.demo.model.Movimentacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
-}
